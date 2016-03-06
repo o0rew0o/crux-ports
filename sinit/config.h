dee7061f2807d132504b1ea446ca1b62a4165130
ix <arcetera@openmailbox.org>
2016-03-06 09:58:30 -0500

    add missing dep

---
+++ sinit/config.h
@@ -0,0 +1,5 @@
+/* See LICENSE file for copyright and license details. */
+
+static char *const rcinitcmd[]     = { "/sbin/init", "--init", NULL };
+static char *const rcrebootcmd[]   = { "/sbin/init", "--reboot", NULL };
+static char *const rcpoweroffcmd[] = { "/sbin/init", "--shutdown", NULL };
